5dd62119a2e190a4f99505947dd6dee0a5d5a5ab
MZena <manuelzenareque@gmail.com>
2022-03-15 16:00:30 -0500

    update config docker+eureka+configserver

--- src/main/java/com/nttdata/MSPayment/MsPaymentApplication.java
+++ src/main/java/com/nttdata/MSPayment/MsPaymentApplication.java
@@ -2,8 +2,10 @@ package com.nttdata.MSPayment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MsPaymentApplication {
 
 	public static void main(String[] args) {
